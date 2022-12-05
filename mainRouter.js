de604d30ee3402cb7844dcf13a0d6b06a0a8d947
נהוראי <nehoraimimon@gmail.com>
2022-12-05 14:14:19 +0200

    server connect with mongoDb

--- mainRouter.js
+++
@@ -1,7 +0,0 @@
-const express = require('express'),
-    router = express.Router(),
-    fileRouter = require("./fileRouter");
-
-router.use("./files", fileRouter)
-
-
--- server.js
+++ server.js
@@ -1,44 +1,54 @@
-const { request } = require('http');
-
 const express = require('express'),
     cors = require('cors'),
     app = express(),
     swaggerFile = require("./swagger/swaggerOutput.json"),
     swaggerUi = require("swagger-ui-express"),
-    multer = require("multer");
-    // mainRouter= require('./mainRouter');
+    multer = require("multer"),
+    fs = require("fs")
+// userRoutes= require('./user/userRoutes');
 
 app.use(cors())
 app.use(express.json())
 
 const PORT = 3000
 
-// app.use("/", mainRouter)
+// app.use("/user", userRoutes)
 
 const fileStorageEngine = multer.diskStorage({
-    destination:(req, file, cb)=>{
-        cb(null,"./files")
+    destination: (req, file, cb) => {
+        cb(null, "./collection")
     },
-    filename:(req, file, cb)=>{
+    filename: (req, file, cb) => {
         cb(null, file.originalname)
     }
 })
 
-const upload = multer({storage: fileStorageEngine})
+const upload = multer({ storage: fileStorageEngine })
 
-app.post("/single", upload.single("file"), (req, res)=>{
-console.log(req.file);
-res.send("single was sent successfully")
+
+//    const upload = multer()
+
+app.post("/single", upload.single("file"), (req, res) => {
+    console.log(req.file);
+    res.send("single was sent successfully")
 });
 
-// TO DO: only single is working, multiple not working
+// TODO: only single is working, multiple not working
 
-app.post('/multiple', upload.array("files",3), (req, res)=>{
-console.log(req.files);
-res.send("multiple was sent successfully")
+app.post('/multiple', upload.array("files"), (req, res) => {
+    console.log(req);
+    console.log(req.file);
+    console.log(req.files);
+    res.send("multiple was sent successfully")
+})
+
+// TODO: bring back only the string..not the file
+
+app.get("/myCollection", (req, res) => {
+    const files = fs.readdirSync("./collection")
 })
 
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerFile))
-
-app.listen(PORT , ()=> console.log("server is runing on port " + PORT ))
+app.listen(PORT, () => console.log("server is runing on port " + PORT))
+require("./db").connect();
 
